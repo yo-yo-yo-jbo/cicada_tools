38bbbf50098aef5a99f6480a2f21b2008b682152
jbo <jbo@jbo>
2024-10-03 19:32:44 -0700

    rephrasing to experiments

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ import sympy
 import subprocess
 import logging
 
-class Attempts(object):
+class Experiments(object):
     """
-        Attempts made.
+        Experiments made.
     """
 
     @staticmethod
@@ -955,9 +955,9 @@ def main():
     logging.basicConfig(filename='CicadaUtils.log', level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    # List all static methods in Attempts
-    attempts = [ (k, v) for (k, v) in Attempts.__dict__.items() if isinstance(v, staticmethod) ]
-    menu_items = [ (k.replace('_', ' ').title(), v.__func__.__doc__.strip().split('\n')[0]) for (k, v) in attempts ]
+    # List all static methods in Experiments 
+    exprs = [ (k, v) for (k, v) in Experiments.__dict__.items() if isinstance(v, staticmethod) ]
+    menu_items = [ (k.replace('_', ' ').title(), v.__func__.__doc__.strip().split('\n')[0]) for (k, v) in exprs ]
 
     # Run forever
     while True:
@@ -974,7 +974,7 @@ def main():
             logger.info(f'Starting: {menu_items[choice][0]}')
             screen.clear()
             screen.print_yellow(f'== {menu_items[choice][0]} ==\n')
-            attempts[choice][1].__func__()
+            exprs[choice][1].__func__()
             logger.info(f'Finished: {menu_items[choice][0]}')
             screen.print_green('\n\nEXECUTION COMPLETE\n')
             screen.press_enter()
