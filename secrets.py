7f469914246bec16500fb645ed536beb1dc0b889
jbo <jbo@jbo>
2024-10-10 19:05:39 -0700

    minor issues

--- secrets.py
+++ secrets.py
@@ -1,5 +1,8 @@
 from sympy import Matrix
 
+# The deep hash
+DEEP_HASH = '36367763ab73783c7af284446c59466b4cd653239a311cb7116d4618dee09a8425893dc7500b464fdaf1672d7bef5e891c6e2274568926a49fb4f45132c2a8b4'
+
 # Important square matrices
 SQUARES = [
 
